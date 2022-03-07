7b959ffc601522b3cf75086d23b058a5c13beb75
Lary S <97912061+larysoul@users.noreply.github.com>
2022-03-07 16:16:17 -0300

    arquivo sql

--- lojagames.sql
+++ lojagames.sql
@@ -3,8 +3,8 @@ use db_lojagames;
 insert into tb_categoria (genero) values ("Survival");
 insert into tb_categoria (genero) values ("FPS");
 insert into tb_categoria (genero) values ("RPG");
-insert into tb_categoria (genero) values ("MOBA");
-insert into tb_categoria (genero) values ("Battle Royale");
+insert into tb_categoria (genero) values ("Indie");
+insert into tb_categoria (genero) values ("Plataforma");
 insert into tb_categoria (genero) values ("Souls-like");
 
 select * from tb_categoria;
@@ -12,19 +12,21 @@ select * from tb_categoria;
 select * from tb_produto;
 
 insert into tb_produto (titulo, descricao, console, quantidade, preco, foto, categoria_id) 
-values ("The Legend of Zelda: Ocarina of Time", "Um jovem chamado Link começa uma jornada para prevenir 
-que Ganondorf consiga obter a Triforce, uma relíquia mágica de poder onipotente. Devido a 
-circunstâncias não previstas, Ganondorf consegue obter parte da Triforce. Através de viagens pelo tempo com a
- mítica Master Sword, Link precisa juntar os Seis Medalhões necessários para derrotar Ganondorf e 
- restaurar a paz em Hyrule.", "3DS", 65, 
-192.99, "https://imgur.com/4YUk0uF", 8);
+values ("Hollow Knight", "Forje seu caminho em Hollow Knight! Uma aventura de ação épica em um vasto reino arruinado de insetos e heróis. Explore cavernas serpenteantes, lute contra criaturas malignas e alie-se a insetos bizarros num estilo clássico 2D desenhado à mão.", 
+"PC", 65, 27.99, "https://imgur.com/tHMwjgI", 5);
 insert into tb_produto (titulo, descricao, console, quantidade, preco, foto, categoria_id) 
-values ("Resident Evil 3 REMAKE", "É uma recriação de Resident Evil 3: Nemesis, lançado em 1999, e o 
-enredo segue Jill Valentine tentando escapar de um apocalipse zumbi enquanto é caçada por um 
-ser biologicamente inteligente conhecido como Nemesis.", "Multiplataforma", 120, 
-129.99, "https://imgur.com/a/uRdrM4W", 1);
+values ("First Class Trouble", "É um jogo de equipe onde os jogadores devem trabalhar juntos e, ao mesmo tempo, contra uns aos outros para sobreviverem a um disastre. O objetivo é desligar a A.I. assassina.", 
+"PS4", 30, 41.00, "https://imgur.com/2VnhUzn", 4);
 insert into tb_produto (titulo, descricao, console, quantidade, preco, foto, categoria_id) 
-values ("Dying Light", "Em vez da paradisíaca ilha de Dead Island, o cenário aqui é uma desolada cidade 
-infestada por zumbis. O jogo é em mundo aberto e tem como característica um gameplay 
-frenético baseado em movimentos de parkour num mapa bem extenso.", "Xbox One", 29, 164.95, 
-"https://imgur.com/l41xGDz", 1);
+values ("Dying Light", "Em vez da paradisíaca ilha de Dead Island, o cenário aqui é uma desolada cidade infestada por zumbis. O jogo é em mundo aberto e tem como característica um gameplay frenético baseado em movimentos de parkour num mapa bem extenso.", 
+"PS4", 58, 164.95, "https://imgur.com/l41xGDz", 1);
+insert into tb_produto (titulo, descricao, console, quantidade, preco, foto, categoria_id) 
+values("Metro Exodus", "Produzido pela 4A Games, Metro Exodus é um jogo de tiro em primeira pessoa impressionante com uma história envolvente. O jogo mistura combates letais e furtividade com exploração e terror de sobrevivência em um dos universos mais imersivos já criados.",
+"PC", 40, 56.00, "https://imgur.com/SXyxuvf", 2);
+insert into tb_produto (titulo, descricao, console, quantidade, preco, foto, categoria_id) 
+values("Demons Souls", "Totalmente reconstruído do zero e aprimorado, este remake apresenta os horrores dessa terra de fantasia sombria e nebulosa a uma nova geração de jogadores.", 
+"PS5", 28, 349.90, "https://imgur.com/DZdcRnX", 6);
+insert into tb_produto (titulo, descricao, console, quantidade, preco, foto, categoria_id) 
+values("GRIS", "Gris é uma jovem esperançosa, perdida em seu próprio mundo, que lida com uma dolorosa experiência.", 
+"PC", 22, 32.99, "https://imgur.com/pN625qY", 4);
+
